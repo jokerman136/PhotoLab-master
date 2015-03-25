364cc58eae218026d73e67e07f6cce1f1e52b6a8
Richard Jones <bedhed50@gmail.com>
2015-03-25 13:42:22 -0600

    FauxToe start (Not Done)

--- src/pixFaux/controller/FauxToeRunner.java
+++ src/pixFaux/controller/FauxToeRunner.java
@@ -3,4 +3,10 @@ package pixFaux.controller;
 public class FauxToeRunner
 {
 
+	public static void main(String[] args)
+	{
+		FauxToeController appController = new FauxToeController();
+		appController.start();
+	}
+
 }
